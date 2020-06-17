561dc45e32458b12bd8ad84dec4a805946d16ee5
AmrAngry <amr.elghadban@gmail.com>
2020-06-17 19:09:41 +0400

    Refactor class name

--- ConcurrencyDemo/BlockOperationViewController.swift
+++ ConcurrencyDemo/BlockOperationViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-let imageURLs = ["http://www.planetware.com/photos-large/F/france-paris-eiffel-tower.jpg", "http://adriatic-lines.com/wp-content/uploads/2015/04/canal-of-Venice.jpg", "https://s1.ax1x.com/2017/12/06/oaiz8.png", "https://s1.ax1x.com/2017/12/06/oakQS.jpg"]
+let imageURLs = ["http://www.planetware.com/photos-large/F/france-paris-eiffel-tower.jpg",
+                 "http://adriatic-lines.com/wp-content/uploads/2015/04/canal-of-Venice.jpg",
+                 "https://s1.ax1x.com/2017/12/06/oaiz8.png",
+                 "https://s1.ax1x.com/2017/12/06/oakQS.jpg"]
 
 class Downloader {
     
